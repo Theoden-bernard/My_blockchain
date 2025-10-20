5ff752ef198508767b27a42b6787e58344e61896
Ewan P <Ewan@iMac-de-pluton-4.local>
2025-10-20 14:30:30 +0200

    REFACTOR: change of printing error message

--- secure_malloc/secure_malloc.c
+++ secure_malloc/secure_malloc.c
@@ -1,4 +1,7 @@
 #ifdef UNIT_TEST
+#endif
+
+#include <unistd.h>
 
 /**
 * @summary malloc the buffer and verify the result of the malloc.
@@ -10,7 +13,8 @@ void* secure_malloc(size_t size)
     char* buff = malloc(size);
     if (buff == NULL)
     {
-        print_error_message(ERR_NO_MORE_RESSOURCES);
+        // print_error_message(ERR_NO_MORE_RESSOURCES);
+        write(1, "no more ressources available\n", 29);
         return NULL;
     }
     memset(buff, 0, size);
