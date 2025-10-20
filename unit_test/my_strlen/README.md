2e41552fae235b0455ba4414b88473dcde4c4329
Ewan P <Ewan@iMac-de-pluton-4.local>
2025-10-20 14:58:37 +0200

    FEAT: added function my_strlen

---
+++ unit_test/my_strlen/README.md
@@ -0,0 +1,18 @@
+# My Strlen
+
+## 📝 Description :
+#### My Strlen is a function that calculates the size of a string. If the string is NULL, returns 1.
+---
+## Parameters :
+- String : array of character that size needs to be calculated.
+---
+## Return :
+- Return an int that is the size of the string.
+- If the string is NULL, returns 1.
+---
+## Exemple : 
+```c
+char* string = "Test\n";
+my_strlen(string);
+```
+size of string = 5;
---
+++ unit_test/my_strlen/my_strlen.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/**
+* @brief recreation of strlen.
+* @param char* string: array of character.
+* @return return the size.
+*/
+int my_strlen(char* string)
+{
+    if (string == NULL)
+    {
+        return 1;
+    }
+
+    int size = 0;
+    while (string[size] != '\0')
+    {
+        size += 1;
+    }
+    return size;
+}
+
+
+#ifdef UNIT_TEST
+int main()
+{
+    char* test = "this is a test\n";
+    printf("%d\n", my_strlen(test));
+
+    return 0;
+}
+#endif
