a3f9e032201d82907091e27ee12dd06b98b8cbf7
EwanPigneret <ewan.pigneret@gmail.com>
2025-10-22 15:33:18 +0200

    FEAT: added function init_my_readline

--- unit_test/init_my_readline/init_my_readline.c
+++ unit_test/init_my_readline/init_my_readline.c
@@ -1,5 +1,9 @@
 #include "blockchain.h"
 
+/**
+ * @brief create a buffer for readline using secure_malloc.
+ * @return the buffer initialised.
+ */
 void* init_my_readline()
 {
     void* buff = secure_malloc(sizeof(char) * READLINE_READ_SIZE);
