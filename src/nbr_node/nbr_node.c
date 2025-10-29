40cea19f6c9a1872d61ebc40f8f84d1bb9eafcb6
EwanPigneret <ewan.pigneret@gmail.com>
2025-10-29 15:17:50 +0100

    FEAT: added unit test for nbr_node

--- src/nbr_node/nbr_node.c
+++ src/nbr_node/nbr_node.c
@@ -21,3 +21,25 @@ int nbr_node(t_list* head)
     
     return index;
 }
+
+#ifdef UNIT_TEST_NBR_NODE
+int main()
+{
+    t_list* head = secure_malloc(sizeof(t_list*));
+    head->nid = 1;
+    head->head_block = secure_malloc(sizeof(t_block*));
+    head->next = secure_malloc(sizeof(t_list*));
+    head->next->nid = 2;
+    head->next->next = secure_malloc(sizeof(t_list*));
+    head->next->next->nid = 3;
+    head->next->next->next = NULL;
+    
+    if (nbr_node(head) == 3)
+    {
+        printf("True\n");
+        return 1;
+    }
+    printf("False\n")
+    return 0;
+}
+#endif
