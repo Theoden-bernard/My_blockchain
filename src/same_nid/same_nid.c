5935033f1aa87d20fd918c7081db8fcc7bd54d1f
EwanPigneret <ewan.pigneret@gmail.com>
2025-10-31 09:30:11 +0100

    FEAT: added unit test for same_nid

--- src/same_nid/same_nid.c
+++ src/same_nid/same_nid.c
@@ -27,3 +27,27 @@ int same_nid (t_list* head, int id)
 
     return same_nid;
 }
+
+#ifdef UNIT_TEST_SAME_NID
+int main()
+{
+    t_list* head = secure_malloc(sizeof(t_list*));
+    head->nid = 1;
+    head->head_block = secure_malloc(sizeof(t_block*));
+    head->head_block->bid = "1";
+    head->head_block->next = NULL;
+    head->next = secure_malloc(sizeof(t_list*));
+    head->next->nid = 2;
+    head->next->next = NULL;
+
+    int nid = 2;
+    
+    if (same_nid(head, nid))
+    {
+        printf("True\n")
+        return 1;
+    }
+    printf("False\n");
+    return 0;
+}
+#endif
